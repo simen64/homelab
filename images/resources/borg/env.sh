8db29f0e3857b87afc5b72a17f8477c2edb651a7
Simen Munch-Olsen <simenmunch@gmail.com>
2025-11-02 16:23:00 +0100

    Revert "chore: rename containerfile to Containerfile"
    This reverts commit 8b00a05ced45b4550d0e993b42f4bde85f8ff5a7.

--- images/resources/borg/env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export BORG_REPO='borg@backup.server:/path/to/repository'
-export BORG_BASE_DIR=/backup
-export BORG_PASSPHRASE="a randomly generated password unique for every system"
-export BORG_RSH='ssh -i /backup/system-name-ssh-key'
