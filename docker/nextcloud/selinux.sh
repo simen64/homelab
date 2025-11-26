440a0c47adab5da67277e58b5e10a351ae95de7d
simen64 <simenmunch@gmail.com>
2025-11-26 11:05:54 +0100

    feat: add selinux and access to dev/urandom

---
+++ docker/nextcloud/selinux.sh
@@ -0,0 +1,10 @@
+semanage fcontext -a -t httpd_sys_rw_content_t '/var/www/html/nextcloud/data(/.*)?'
+semanage fcontext -a -t httpd_sys_rw_content_t '/var/www/html/nextcloud/config(/.*)?'
+semanage fcontext -a -t httpd_sys_rw_content_t '/var/www/html/nextcloud/apps(/.*)?'
+semanage fcontext -a -t httpd_sys_rw_content_t '/var/www/html/nextcloud/.htaccess'
+semanage fcontext -a -t httpd_sys_rw_content_t '/var/www/html/nextcloud/.user.ini'
+semanage fcontext -a -t httpd_sys_rw_content_t '/var/www/html/nextcloud/3rdparty/aws/aws-sdk-php/src/data/logs(/.*)?'
+
+restorecon -Rv '/var/www/html/nextcloud/'
+
+setsebool -P httpd_can_sendmail on
